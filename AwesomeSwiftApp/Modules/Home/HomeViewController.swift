1f6375aee0f52328c8e1b1cbfb12d40aa1b6d939
chacha <867571123@qq.com>
2019-12-17 17:57:53 +0800

    Init HomeView and HomeView controller

---
+++ AwesomeSwiftApp/Modules/Home/HomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.swift
+//  AwesomeSwiftApp
+//
+//  Created by chacha on 2019/12/17.
+//  Copyright © 2019 Chacha. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+}
