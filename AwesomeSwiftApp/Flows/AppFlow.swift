c87e8eeaa0679fdb8b2b6b575e7c8eba5e9d01fd
chacha <867571123@qq.com>
2020-09-27 15:54:06 +0800

    feat: add AppFlow and AppServices

---
+++ AwesomeSwiftApp/Flows/AppFlow.swift
@@ -0,0 +1,41 @@
+//
+//  AppFlow.swift
+//  AwesomeSwiftApp
+//
+//  Created by chacha on 2020/9/26.
+//  Copyright © 2020 Chacha. All rights reserved.
+//
+
+import Foundation
+import RxFlow
+import RxCocoa
+import RxSwift
+import UIKit
+
+class AppFlow: Flow {
+    var root: Presentable {
+        self.rootViewController
+    }
+    
+    private lazy var rootViewController: UINavigationController = {
+        let viewCtrl = UINavigationController()
+        return viewCtrl
+    }()
+
+    func navigate(to step: Step) -> FlowContributors {
+        switch step {
+        default:
+            return .none
+        }
+    }
+
+    private let services: AppServices
+    
+    init(services: AppServices) {
+        self.services = services
+    }
+    
+    deinit {
+        print("\(type(of: self)): \(#function)")
+    }
+}
---
+++ AwesomeSwiftApp/Services/AppServices.swift
@@ -0,0 +1,11 @@
+//
+//  AppServices.swift
+//  AwesomeSwiftApp
+//
+//  Created by chacha on 2020/9/26.
+//  Copyright © 2020 Chacha. All rights reserved.
+//
+
+struct AppServices {
+    let demoService: Any
+}
