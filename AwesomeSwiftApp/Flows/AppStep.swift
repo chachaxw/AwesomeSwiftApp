1eb12683c18da877f009ce2fa41acdb8631938cd
chacha <867571123@qq.com>
2020-10-12 22:13:27 +0800

    feat: add AppStep enum

---
+++ AwesomeSwiftApp/Flows/AppStep.swift
@@ -0,0 +1,32 @@
+//
+//  AppStep.swift
+//  AwesomeSwiftApp
+//
+//  Created by chacha on 2020/9/29.
+//  Copyright © 2020 Chacha. All rights reserved.
+//
+
+import Foundation
+import RxFlow
+
+enum AppStep: Step {
+    // Global
+    case logoutIsRequired
+    case unauthorized
+    
+    // Api Key
+    case apiKeyIsRequired
+    case apiKeyIsFilledIn
+
+    // Login
+    case loginIsRequired
+    case userIsLoggedIn
+    
+    // Settings
+    case settingsAreRequired
+    case settingsAreComplete
+    
+    // About
+    case aboutIsRequired
+    case aboutIsComplete
+}
