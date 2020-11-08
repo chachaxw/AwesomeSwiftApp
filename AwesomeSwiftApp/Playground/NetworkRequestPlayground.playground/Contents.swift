8a115cfbfb1382d4f80927fa784e2611903c225d
chacha <867571123@qq.com>
2020-11-08 15:28:01 +0800

    feat: add network request playground, add DispatchGroup examples

---
+++ AwesomeSwiftApp/Playground/NetworkRequestPlayground.playground/Contents.swift
@@ -0,0 +1,97 @@
+import UIKit
+
+var str = "Hello, network request playground"
+
+/// 使用GCD多线程处理网络请求的正确做法: A、B都定义成异步任务，在并行队列中嵌套异步任务，
+/// 最后切换到主队列去刷新UI，这样做界面可以保证最流畅。
+///
+// 创建并行队列，尽量用自定义队列，免得自己的代码质量不过关，影响全局队列
+let queue = DispatchQueue(label: "chacha.request", attributes: .concurrent)
+
+//queue.async {
+//
+//    print("开始请求数据 \(Date()) thread: \(Thread.current)")
+//    sleep(5) // 开始模拟网络请求
+//    print("请求数据完成 \(Date()) thread: \(Thread.current)")
+//
+//    // 异步执行
+//    queue.async {
+//        print("开始处理数据 \(Date()) thread: \(Thread.current)")
+//        sleep(5) // 开始模拟网络请求
+//        print("数据处理完成 \(Date()) thread: \(Thread.current)")
+//
+//        // 切换到主队列，更新UI
+//        DispatchQueue.main.async {
+//            print("UI更新成功 \(Date()) thread: \(Thread.current)")
+//        }
+//    }
+//}
+//
+/// 上述代码请求结果:
+/// 开始请求数据 2020-11-08 06:09:26 +0000 thread: <NSThread: 0x600001505c00>{number = 5, name = (null)}
+/// 请求数据完成 2020-11-08 06:09:31 +0000 thread: <NSThread: 0x600001505c00>{number = 5, name = (null)}
+/// 开始处理数据 2020-11-08 06:09:31 +0000 thread: <NSThread: 0x60000151c480>{number = 3, name = (null)}
+/// 数据处理完成 2020-11-08 06:09:36 +0000 thread: <NSThread: 0x60000151c480>{number = 3, name = (null)}
+/// UI更新成功 2020-11-08 06:09:36 +0000 thread: <NSThread: 0x60000150c7c0>{number = 1, name = main}
+
+/// 使用DispatchGroup
+/// DispatchGroup常用的方法：
+/// 1. group.wait(): 阻塞当前线程，一直到group所有任务执行完毕
+/// 2. group.notify(): 所有任务执行完毕后，异步发送通知，不阻塞当前线程
+///
+
+/// 使用group.notify()改写上面例子：
+let group = DispatchGroup()
+
+queue.async(group: group) {
+    print("开始请求数据 \(Date()) thread: \(Thread.current)")
+    sleep(5) // 开始模拟网络请求
+    print("请求数据完成 \(Date()) thread: \(Thread.current)")
+
+    // 异步执行
+    queue.async(group: group) {
+        print("开始处理数据 \(Date()) thread: \(Thread.current)")
+        sleep(5) // 开始模拟网络请求
+        print("数据处理完成 \(Date()) thread: \(Thread.current)")
+    }
+}
+
+
+print("开始监听")
+
+/// 在当前队列监听，有两种方法：
+/// 第一种
+//group.notify(queue: queue) {
+//    // 切换到主队列，更新UI
+//    DispatchQueue.main.async {
+//        print("UI更新成功 \(Date()) thread: \(Thread.current)")
+//    }
+//}
+
+/// 第二种方法更为简洁，推荐第二种
+group.notify(queue: DispatchQueue.main) {
+    print("UI更新成功 \(Date()) thread: \(Thread.current)")
+}
+
+group.wait() // 阻塞当前线程
+
+print("监听完毕")
+
+/// 上述代码请求结果:
+/// 开始监听
+/// 监听完毕
+/// 开始请求数据 2020-11-08 06:22:58 +0000 thread: <NSThread: 0x6000039f9f40>{number = 5, name = (null)}
+/// 请求数据完成 2020-11-08 06:23:03 +0000 thread: <NSThread: 0x6000039f9f40>{number = 5, name = (null)}
+/// 开始处理数据 2020-11-08 06:23:03 +0000 thread: <NSThread: 0x6000039fa5c0>{number = 4, name = (null)}
+/// 数据处理完成 2020-11-08 06:23:08 +0000 thread: <NSThread: 0x6000039fa5c0>{number = 4, name = (null)}
+/// UI更新成功 2020-11-08 06:23:08 +0000 thread: <NSThread: 0x6000039f8080>{number = 1, name = main}
+///
+/// 如果用group.wait()阻塞当前线程，结果为:
+/// 开始监听
+/// 开始请求数据 2020-11-08 07:11:19 +0000 thread: <NSThread: 0x6000032ea480>{number = 6, name = (null)}
+/// 请求数据完成 2020-11-08 07:11:24 +0000 thread: <NSThread: 0x6000032ea480>{number = 6, name = (null)}
+/// 开始处理数据 2020-11-08 07:11:24 +0000 thread: <NSThread: 0x6000032ea580>{number = 5, name = (null)}
+/// 数据处理完成 2020-11-08 07:11:29 +0000 thread: <NSThread: 0x6000032ea580>{number = 5, name = (null)}
+/// 监听完毕
+/// UI更新成功 2020-11-08 07:11:29 +0000 thread: <NSThread: 0x6000032ec280>{number = 1, name = main}
+///
--- AwesomeSwiftApp/Playground/ThreadPlayground.playground/Contents.swift
+++ AwesomeSwiftApp/Playground/ThreadPlayground.playground/Contents.swift
@@ -1,10 +1,9 @@
-import UIKit
 import Foundation
-
-/// Swift 多线程学习(https://juejin.im/post/6858126631760986126)
+import UIKit
 
 var str = "Hello, thread playground"
 
+/// Swift 多线程学习(https://juejin.im/post/6858126631760986126)
 /// 基础
 ///
 /// 调度工作项：其实就是一项任务，可以把你想要执行的代码写成闭包，
@@ -23,38 +22,58 @@ let item2 = DispatchWorkItem(qos: .userInteractive, flags: .barrier) {
 }
 
 let item3 = DispatchWorkItem {
-    for item in 0...2 {
+    for item in 3...5 {
         print("item3 -> \(item) thread: \(Thread.current)")
     }
 }
 
+let item4 = DispatchWorkItem {
+    for item in 6...8 {
+        print("item4 -> \(item) thread: \(Thread.current)")
+    }
+}
+
 /// DispatchQueue有三种类型:
-///     1. Main queue
-///     2. Global queue
-///     3. Custom queue
+///     1. Main queue（主队列）
+///     2. Global queue（全局队列）
+///     3. Custom queue（自定义队列）
 
 /// 异步执行
 /// NOTE: 在串行队列中执行异步任务，结果跟执行同步任务完全一样
 ///
 /// 1. 主队列追加异步任务，按顺序打印
+
+print("=> 开始执行")
+
 let mainQueue = DispatchQueue.main
 mainQueue.async(execute: item1)
-mainQueue.async(execute: item2)
+//mainQueue.async(execute: item2)
+
+print("=> 执行完毕1")
 
 /// 2. 全局队列追加异步任务，随机打印
 let globalQueue = DispatchQueue.global()
 //globalQueue.async(execute: item1)
 //globalQueue.async(execute: item2)
+globalQueue.sync(execute: item2) // 同步任务
+
+print("=> 执行完毕2")
 
 /// 3. 自定义串行队列追加异步任务，按顺序打印
 let serialQueue = DispatchQueue(label: "serial")
 //serialQueue.async(execute: item1)
 //serialQueue.async(execute: item2)
+serialQueue.sync(execute: item3) // 同步任务
+
+print("=> 执行完毕3")
 
 /// 4. 自定义并行队列追加异步任务，随机打印
 let concurrentQueue = DispatchQueue(label: "concurrent", attributes: .concurrent)
 //concurrentQueue.async(execute: item1)
 //concurrentQueue.async(execute: item2)
+concurrentQueue.sync(execute: item4) // 同步任务
+
+print("=> 执行完毕4")
 
 /// 同步执行
 /// NOTE: 在并行队列中执行同步任务，跟在串行队列中执行异步或同步任务，结果完全一样
@@ -108,3 +127,93 @@ let concurrentQueue = DispatchQueue(label: "concurrent", attributes: .concurrent
 /// 从时间上看B执行完才能执行A；从空间上看，A执行完才能执行B，于是就引起了死锁，导致程序卡死崩溃
 ///
 /// 2. 其他队列死锁
+/// 2.1 自定义串行队列嵌套同步任务，会引起死锁
+
+// 下面代码会引起死锁
+//serialQueue.sync {
+//    print("同步执行 thread \(Thread.current)")
+//    serialQueue.sync {
+//        print("同步执行 thread \(Thread.current)")
+//    }
+//}
+
+// 下面代码会引起死锁
+//serialQueue.async {
+//    print("异步执行 thread \(Thread.current)")
+//    serialQueue.sync {
+//        print("同步执行 thread \(Thread.current)")
+//    }
+//}
+
+// 下面代码不会死锁
+//serialQueue.sync {
+//    print("同步执行 thread \(Thread.current)")
+//    serialQueue.async {
+//        print("异步执行 thread \(Thread.current)")
+//    }
+//}
+
+// 下面代码不会死锁
+//serialQueue.async {
+//    print("异步执行 thread \(Thread.current)")
+//    serialQueue.async {
+//        print("异步执行 thread \(Thread.current)")
+//    }
+//}
+
+/// 2.2 并行队列嵌套同步任务，不会引起死锁
+//concurrentQueue.async {
+//    print("异步执行1 thread \(Thread.current)")
+//    concurrentQueue.sync {
+//        print("同步执行1 thread \(Thread.current)")
+//    }
+//}
+//
+//concurrentQueue.sync {
+//    print("同步执行2 thread \(Thread.current)")
+//    concurrentQueue.sync {
+//        print("同步执行2 thread \(Thread.current)")
+//    }
+//}
+//
+//concurrentQueue.sync {
+//    print("同步执行3 thread \(Thread.current)")
+//    concurrentQueue.async {
+//        print("异步执行3 thread \(Thread.current)")
+//    }
+//}
+//
+//concurrentQueue.async {
+//    print("异步执行4 thread \(Thread.current)")
+//    concurrentQueue.async {
+//        print("异步执行4 thread \(Thread.current)")
+//    }
+//}
+
+/// 死锁核心原因
+/// 1. 主队列上的所有任务（只有可能是异步任务）和其他队列的同步任务都运行在主线程上（主线程有且只有一个）
+/// 2. 线程不在乎任务是同步还是异步，只有队列在乎
+/// 3. 线程不会死锁，只有队列才会死锁
+///
+
+/// 主队列添加同步任务会造成死锁的根本原因:
+/// 1. 主队列只能运行主线程
+/// 2. 主队列无法开启后台线程去干别的事情
+/// 3. 主队列一旦混入同步任务，就会跟已存在的异步任务相互等待，导致死锁
+///
+
+/// 自定义串行队列添加同步任务不会死锁，因为：
+/// 自定义串行队列有能力开启主线程或后台线程(只能启动一个后台线程)。自定义串行队列遇到同步任务，会自动安排到主线程执行；
+/// 遇到异步任务，自动安排在后台线程执行，所以不会死锁。
+///
+
+/// 并行队列添加同步任务不会死锁，因为：
+/// 并行队列有能力启动主线程和后台线程（可以启动一个或多个后台线程，部分设备上可以启动多达64个后台线程）。
+/// 并行队列遇到同步任务，会自动安排到主线程执行；遇到异步任务，自动安排在后台线程执行，所以不会死锁。
+///
+
+/// 自定义串行队列一个异步或同步任务（A）嵌套另一个同步任务（B）会引起死锁，因为：
+/// A、B任务等效为：A1 -> B -> A2，B是同步任务，B在A1之后，A2之前，B必须等A2执行完才能执行，
+/// A2必须等B执行完才能执行，A2执行完才算A1执行完了，逻辑上已经陷入死循环，两者互相等待，导致死锁。
+/// 所以串行队列不能嵌套同步任务，否则会引起死锁。
+///
